b8bafdc6dc5a1f2c6b1f3ecd59ae0b514bdf07f7
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-07-04 13:00:25 -0300

    adicionada configuração de clock do módulo GPIO3

--- genius/src/control_module.c
+++ genius/src/control_module.c
@@ -28,10 +28,16 @@ unsigned int cmGetCtrlModule(CONTROL_MODULE offset){
    	return(HWREG(SOC_CONTROL_REGS + offset));
 }
 
+/*
+ * ===  FUNCTION  ======================================================================
+ *         Name:  disableWdt
+ *  Description:  disable BeagleBone Black WatchDog
+ * =====================================================================================
+ */
 void disableWdt ( ){
 	HWREG(SOC_WDT_1_REGS + WDT_WSPR) = 0xAAAA;
 	while ((HWREG(SOC_WDT_1_REGS + WDT_WWPS) & (1<<4)) != 0 ) { }
 	HWREG(SOC_WDT_1_REGS + WDT_WSPR) = 0x5555;
 	while ((HWREG(SOC_WDT_1_REGS + WDT_WWPS) & (1<<4)) != 0 ) { }
-}
+}/* ----------  end of function watchdog  ---------- */
 
